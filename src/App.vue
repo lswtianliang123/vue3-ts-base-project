527a0fc2212ee83c31a2edaed3e2dee58e4027f0
刘世伟 <lswtianliang@163.com>
2023-03-13 14:11:46 +0800

    feat: 334

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue';
-
-let a = 3;
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-function f1() {
-  console.log();
-}
+
 createApp(App).mount('#app');
