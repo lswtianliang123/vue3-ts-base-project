97bdeeb624dcee3b1352b2fe08161c1e46bc4db4
刘世伟 <lswtianliang@163.com>
2023-03-13 16:49:15 +0800

    feat:34

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-function f312() {
-  //todo
-}
+
+// function f31222() {
+//   //todo
+// }
 createApp(App).mount('#app');
